9f91e30dd6ff4c94efbc04459cec0052593558e2
Tanishk36 <tanishkdhiman19@gmail.com>
2026-05-03 14:52:03 +0530

    move homr contoller to /api/

---
+++ backend/src/main/java/com/taskmanager/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Task Manager API is LIVE 🚀";
+    }
+}
